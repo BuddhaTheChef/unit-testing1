06eac79a5fe83b39e5c866047cb8ee670a3bae60
Aj Wietecha II <ajwietechaii@yahoo.com>
2019-04-09 12:59:08 -0700

    added in expectations for all specs

--- simpleCalculator/calculator.spec.js
+++ simpleCalculator/calculator.spec.js
@@ -1,18 +1,36 @@
 //Suite is a group of Specs(tests)
 //Suites lets you organize specs
+//Specs holds expectations
 
 describe('calculator.js', () => {
+
   it('should add numbers to total', ()=> {
-
+    const calculator = new Calculator();
+    //expect total to be 5
+    calculator.add(5)
+    expect(calculator.total).toBe(5)
   });
+
   it('should subtract numbers to total', ()=> {
-
+    const calculator = new Calculator();
+    calculator.total = 10;
+    calculator.subtract(5)
+    expect(calculator.total).toBe(5)
   });
+
   it('should multiply numbers to total', ()=> {
-
+     const calculator = new Calculator();
+     calculator.total = 100;
+     calculator.multiply(2);
+     expect(calculator.total).toBe(200);
+    
   }); 
-  it('should divide numbers to total', ()=> {
 
+  it('should divide numbers to total', ()=> {
+    const calculator = new Calculator();
+    calculator.total = 15;
+    calculator.divide(5);
+    expect(calculator.total).toBe(3);
   });
 
 });

e485a0bc8c9577965237d1e3a6e421a7569342ff
Aj Wietecha II <ajwietechaii@yahoo.com>
2019-04-10 12:21:21 -0700

    comparing matchers to find diffrences

--- simpleCalculator/calculator.spec.js
+++ simpleCalculator/calculator.spec.js
@@ -5,6 +5,9 @@
 //to disable a spec just put an x in front of it
 //to disable a suite (all specs) just put an x in front of describe
 
+//a matcher is just a function(boolean comparison between an expected value vs the actual value)
+// matcher is responsible to report to jasmine if the expected values is a passing or failing spec
+
 describe('calculator.js', () => {
 
   it('should add numbers to total', ()=> {
@@ -35,5 +38,12 @@ describe('calculator.js', () => {
     calculator.divide(5);
     expect(calculator.total).toBe(3);
   });
+
+  it('has constructor', () => {
+    const calculator = new Calculator();
+    const calculator2 = new Calculator();
+    //toBe() wont work like toEqual() because toEqual compares if the refrences are the same
+    expect(calculator).toEqual(calculator2);
+  })
 
 });

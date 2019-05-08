35c6ea908181f67113911bd3b6d52c8adeccbc31
Aj Wietecha II <ajwietechaii@yahoo.com>
2019-05-07 17:26:17 -0700

    added in packages

--- simpleCalculator/karma.conf.js
+++ simpleCalculator/karma.conf.js
@@ -1,15 +1,24 @@
 module.exports = function(config) {
     config.set({
         frameworks: ['jasmine', 'jasmine-matchers'],
+        preprocessers: {
+            '*.js': ['coverage']
+        },
         files: [
             './custom-matchers.js',
             '*.js',
             '*.spec.js'
         ],
-        plugins: ['karma-jasmine', 'karma-jasmine-matchers', 'karma-chrome-launcher'],
-        reporters: ['dots'],
+        plugins: ['karma-jasmine', 'karma-jasmine-matchers', 'karma-chrome-launcher', 'karma-coverage'],
+        reporters: ['dots', 'coverage'],
         color: true,
         browsers: ['ChromeHeadless'],
-        singleRun: true
+        singleRun: true,
+        coverageReporter: {
+            dir: 'coverage/',
+            reporters: [
+                {type: 'html', subdir: 'html'}
+            ]
+        }
     })
 }
